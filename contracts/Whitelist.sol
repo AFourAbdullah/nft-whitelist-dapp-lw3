d14219755375b0852bdcebe665b2dd9eb602fb27
Abdullah Aziz <abdullahazees4@gmail.com>
2023-08-15 16:35:13 +0500

    project initialized with whitelist contract and its deploy script

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract Whitelist{
+    uint8 public maxNumOfWhitelistedAddressAllowed;
+    uint8 public numOfWhitelistedAddress;
+    mapping (address => bool) isWhitelisted;
+    constructor(uint8 _maxNumOfWhitelistedAddressAllowe){
+        maxNumOfWhitelistedAddressAllowed=_maxNumOfWhitelistedAddressAllowe;
+    }
+    function addAddressToWhitelist() public  {
+        require(!isWhitelisted[msg.sender],"Address is already in whitelist!!");
+        require(numOfWhitelistedAddress<maxNumOfWhitelistedAddressAllowed,"No more address allowed to be whitelisted!");
+        isWhitelisted[msg.sender]=true;
+        numOfWhitelistedAddress+=1;
+    }
+}
